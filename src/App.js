d6425eec900279416e1d1d2c9cff0afdf8311ae9
AJ Speller <ajspeller@gmail.com>
2019-05-11 13:02:29 -0400

    Verify that components are display temporary data during development

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import './App.css';
 
+import { recipes } from './tempList';
+
+import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';
+
 function App() {
-  return <div className='App'>Welcome</div>;
+  return (
+    <React.Fragment>
+      <RecipeList />
+      <RecipeDetails />
+    </React.Fragment>
+  );
 }
 
 export default App;
